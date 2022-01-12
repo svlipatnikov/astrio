aa63331fa0cb53b6892bf399cfc42ec97c77e45d
Sergey Lipatnikov <sl@lmx.su>
2022-01-12 22:22:15 +0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import logo from "./logo.svg";
-import style from "./App.module.scss";
+import style from './App.module.scss';
 
-function App() {
-  return (
-    <div className={style.App}>
-      <header className={style["App-header"]}>
-        <img src={logo} className={style["App-logo"]} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={style["App-link"]}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className={style.App}>TEST</div>;
+};
 
 export default App;
